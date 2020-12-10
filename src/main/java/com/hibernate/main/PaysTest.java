0102f8f4d326e3c3bdb96550a129f834ff0103a4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-10 16:03:14 +0100

    fin

--- src/main/java/com/hibernate/main/PaysTest.java
+++ src/main/java/com/hibernate/main/PaysTest.java
@@ -18,7 +18,7 @@ public class PaysTest {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory(); //initialisation unique
 		Session session = sessionFactory.getCurrentSession(); //pointer sur la session actuelle
 		Pays p1=new Pays("Qatar", "Asie", 64000, 6800000);
-		Pays p2=new Pays("USA", "Amérique",100000, 420000000);
+		Pays p2=new Pays("Canda", "Amérique",100000, 120000000);
 		Pays p3=new Pays("France","Europe", 643801, 68809000);
 		Pays p4 =new Pays("Tunisie", "Afrique", 410000, 12200000);
 	
@@ -26,6 +26,30 @@ public class PaysTest {
 		{
 		Transaction tx = session.beginTransaction();
 		Query query ;
+		query = session.createQuery("select count(nom) from Pays WHERE nom=:nom");
+		query.setParameter("nom",p2.getNom());
+		long nbo = (Long) query.uniqueResult();
+		if(nbo==0)
+		session.save(p2);
+		else
+			System.out.println("le pays"+p2.getNom()+" existe déjà");
+		query = session.createQuery("select count(nom) from Pays WHERE continent='europe'");
+				long nbp = (Long) query.uniqueResult();
+				System.out.println("nombre de pays Européen= "+nbp);
+				query = session.createQuery("select min(population) from Pays");
+				long nbmin = (Long) query.uniqueResult();
+				System.out.println("nombre minimale de population= "+nbmin);
+				query = session.createQuery("select max(population),nom from Pays");
+				// query= session.createQuery("from Pays  ");
+					// query.setParameter("continent","europe");
+				    List l = query.list();
+
+				Object resultat[] = (Object[]) l.get(0) ;
+			    Long max = (Long) resultat[0];
+			    String np= (String) resultat[1];
+
+
+						System.out.println("nombre maximale de population= "+max.longValue()+" "+np.toString());
 		/*
 	session.save(p1);
 	session.save(p2);
