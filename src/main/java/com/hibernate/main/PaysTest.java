2584a13892851918147c317158273af0ce07b36f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-10 14:02:34 +0100

    hql

--- src/main/java/com/hibernate/main/PaysTest.java
+++ src/main/java/com/hibernate/main/PaysTest.java
@@ -17,33 +17,51 @@ public class PaysTest {
 		// TODO Auto-generated method stub
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory(); //initialisation unique
 		Session session = sessionFactory.getCurrentSession(); //pointer sur la session actuelle
+		Pays p1=new Pays("Qatar", "Asie", 64000, 6800000);
+		Pays p2=new Pays("USA", "Amérique",100000, 420000000);
+		Pays p3=new Pays("France","Europe", 643801, 68809000);
+		Pays p4 =new Pays("Tunisie", "Afrique", 410000, 12200000);
 	
 		try
 		{
 		Transaction tx = session.beginTransaction();
 		Query query ;
-	 query= session.createQuery("from Pays WHERE continent =:continent");
-	 query.setParameter("continent","europe");
-		List<Pays> paysList = query.list();
-		for(Pays p : paysList){
-			//System.out.println("List de pays::"+p.getId()+","+p.getNom()+","+p.getContinent());
-			System.out.println(p.toString());
-		}
+		/*
+	session.save(p1);
+	session.save(p2);
+	session.save(p3);
+	session.save(p4);
+	*/
+		query = session.createQuery("update Pays set nom= :name where id= :id");
+				query.setParameter("name", "Etat Unis");
+				query.setLong("id", 2);
+				int result = query.executeUpdate();
+				System.out.println("Pays mis à jour="+result);
+		
+				
+				query = session.createQuery("delete from Pays where id= :id");
+				query.setLong("id", 2);
+				result = query.executeUpdate();
+				System.out.println("Pays supprimé="+result);
 		 query= session.createQuery("from Pays  ");
 		// query.setParameter("continent","europe");
-			List<Pays> paysList2 = query.list();
-			for(Pays p : paysList2){
+			List<Pays> paysList = query.list();
+			for(Pays p : paysList){
 				//System.out.println("List de pays::"+p.getId()+","+p.getNom()+","+p.getContinent());
 				System.out.println(p.toString());
 			}
+		
+			session.flush();
+			session.getTransaction().commit();
+
 		}
 		
 		catch(Exception ex)
 		{
 			System.out.println("exception: "+ex);
-
 		}
 		sessionFactory.close();
+
 	}
 
 }
